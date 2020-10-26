aa0d87e9a122f9b0af856d8fc79e4ca71fea326f
David Bueno Castro <david.bueno.castro@iesjandula.es>
2020-10-26 18:10:07 +0100

    hecho swiper

--- peliculas/lib/src/pages/home_page.dart
+++ peliculas/lib/src/pages/home_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_swiper/flutter_swiper.dart';
 
 class HomePage extends StatelessWidget {
   @override
@@ -21,6 +22,23 @@ class HomePage extends StatelessWidget {
   }
 
   Widget _swiperTarjetas() {
-    return Container();
+    return Container(
+      width: double.infinity,
+      height: 300.0,
+      padding: EdgeInsets.only(top: 10.0),
+      child: Swiper(
+        itemBuilder: (BuildContext context, int index) {
+          return Image.network(
+            'https://es.web.img3.acsta.net/pictures/20/01/22/17/09/1712711.jpg',
+            fit: BoxFit.fill,
+          );
+        },
+        itemCount: 3,
+        pagination: new SwiperPagination(),
+        control: new SwiperControl(),
+        layout: SwiperLayout.STACK,
+        itemWidth: 200.0,
+      ),
+    );
   }
 }
