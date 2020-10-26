c2f9e8c5db3d5115d89d159719fcb94294b35bc9
David Bueno Castro <david.bueno.castro@iesjandula.es>
2020-10-26 17:55:46 +0100

    añadido hasta el flutter swiper en el pubspec.yaml

--- peliculas/lib/main.dart
+++ peliculas/lib/main.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 
-import 'package:flutter/material.dart';
 import 'package:peliculas/src/pages/home_page.dart';
 
 void main() => runApp(MyApp());
@@ -9,6 +8,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       title: 'Películas',
       initialRoute: '/',
       routes: {
--- peliculas/lib/src/pages/home_page.dart
+++ peliculas/lib/src/pages/home_page.dart
@@ -3,6 +3,24 @@ import 'package:flutter/material.dart';
 class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Peliculas en cines'),
+        backgroundColor: Colors.indigoAccent,
+        actions: [Icon(Icons.search)],
+        centerTitle: false,
+      ),
+      body: Container(
+        child: Column(
+          children: [
+            _swiperTarjetas(),
+          ],
+        ),
+      ),
+    );
+  }
+
+  Widget _swiperTarjetas() {
     return Container();
   }
 }
